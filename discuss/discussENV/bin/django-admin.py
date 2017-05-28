b8f33e9ccf474b2172ed700b960f1176f453c919
anjuncc <anjuncc@163.com>
2017-05-28 15:59:38 +0800

    init

---
+++ discuss/discussENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anjun/py/github.com/anjunact/djprojects/discuss/discussENV/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mmdb/mmdbENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anjun/py/github.com/anjunact/djprojects/mmdb/mmdbENV/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
